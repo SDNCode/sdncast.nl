467894b2ad62b5a2c14c0cf915e80d849d082567
Maarten van Stam <maarten@vanstam.org>
2018-07-16 23:42:29 +0200

    Fixed Gulp Processing, set css on par with standards, updated packages

--- src/sdncast.nl/gulpfile.js
+++ src/sdncast.nl/gulpfile.js
@@ -1,64 +1,67 @@
 /// <binding AfterBuild='default' Clean='clean' />
+
+"use strict";
+
 var gulp = require("gulp"),
-    del = require("del"),
     concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    jshint = require('gulp-jshint'),
     csslint = require('gulp-csslint'),
+    cssmin = require("gulp-cssmin"),
+    jshint = require('gulp-jshint'),
+    uglify = require("gulp-uglify"),
+    rimraf = require("rimraf"),
     path = require("path");
 
-var paths = {
-    webroot: "./wwwroot/"
-};
+var webroot = "./wwwroot/"; 
 
 var library = {
-  base: "node_modules",
-  destination: "lib",
-  source: [
-    // glob pattern to get the dirname and match only js and min.js file wanted
-    path.dirname(require.resolve('jquery-validation-unobtrusive/jquery.validate.unobtrusive.js')) + '/*unobtrusive**.js',
-    // alternative of declaring each file
-    require.resolve('bootstrap/dist/js/bootstrap.js'),
-    require.resolve('bootstrap/dist/js/bootstrap.min.js'),
-    require.resolve('bootstrap/dist/css/bootstrap.css'),
-    require.resolve('bootstrap/dist/css/bootstrap.min.css'),
-    // glob pattern to get all files within the directory
-    path.dirname(require.resolve('bootstrap/dist/fonts/glyphicons-halflings-regular.woff')) + '/**',
-    // declare each file
-    require.resolve('jquery/dist/jquery.js'),
-    require.resolve('jquery/dist/jquery.min.js'),
-    // only one file is distributed
-    require.resolve('jquery-validation/dist/jquery.validate.js')
-  ]
-}
+    base: "node_modules",
+    destination: "lib",
+    source: [
+        // glob pattern to get the dirname and match only js and min.js file wanted
+        path.dirname(require.resolve('jquery-validation-unobtrusive/jquery.validate.unobtrusive.js')) + '/*unobtrusive**.js',
+        // alternative of declaring each file
+        require.resolve('bootstrap/dist/js/bootstrap.js'),
+        require.resolve('bootstrap/dist/js/bootstrap.min.js'),
+        require.resolve('bootstrap/dist/css/bootstrap.css'),
+        require.resolve('bootstrap/dist/css/bootstrap.min.css'),
+        // glob pattern to get all files within the directory
+        path.dirname(require.resolve('bootstrap/dist/fonts/glyphicons-halflings-regular.woff')) + '/**',
+        // declare each file
+        require.resolve('jquery/dist/jquery.js'),
+        require.resolve('jquery/dist/jquery.min.js'),
+        // only one file is distributed
+        require.resolve('jquery-validation/dist/jquery.validate.js')
+    ]
+};
 
-paths.library = paths.webroot + library.destination;
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site-min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+var paths = {
+    library: webroot + library.destination,
+    js: webroot + "js/**/*.js",
+    minJs: webroot + "js/**/*.min.js",
+    css: webroot + "css/**/*.css",
+    minCss: webroot + "css/**/*.min.css",
+    concatJsDest: webroot + "js/site.min.js",
+    concatCssDest: webroot + "css/site.min.css"
+};
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean:lib", function (cb) {
+    rimraf(paths.library, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:lib"]);
 
 gulp.task("lib", ["clean"], function () {
   return gulp.src(library.source, { base: library.base })
     .pipe(gulp.dest(paths.library));
 });
-
-gulp.task("clean:lib", function () {
-    return del(paths.library);
-});
-
-gulp.task("clean:js", function () {
-    return(del(paths.concatJsDest));
-});
-
-gulp.task("clean:css", function () {
-    return(del(paths.concatCssDest));
-});
-
-gulp.task("clean", ["clean:js", "clean:css", "clean:lib"]);
 
 gulp.task("min:js", function () {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
@@ -76,16 +79,16 @@ gulp.task("min:css", function () {
 
 gulp.task("min", ["lib", "csslint", "jshint", "min:js", "min:css"]);
 
-gulp.task("jshint", ["lib"], function() {
+gulp.task("jshint", ["lib"], function(cb) {
     return gulp.src([paths.js, "!" + paths.minJs])
         .pipe(jshint())
-        .pipe(jshint.reporter())
+        .pipe(jshint.reporter(), cb);
 });
 
 gulp.task("csslint", ["lib"], function() {
     return gulp.src([paths.css, "!"  + paths.minCss])
         .pipe(csslint())
-        .pipe(csslint.reporter());
+        .pipe(csslint.formatter());
 });
 
 gulp.task("prepublish", ["lib", "csslint", "jshint",  "min"]);
