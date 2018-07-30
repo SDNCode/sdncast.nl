45f7e92aba384f60c65cfce7cfc326d8666edf9b
Maarten van Stam <maarten@vanstam.org>
2018-07-31 01:53:39 +0200

    Added DotNetConf Page Starter

---
+++ src/sdncast.nl/Pages/DotNetConf/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace sdncast.nl.Pages.DotNetConf
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppSettings _appSettings;
+        private readonly IHostingEnvironment _env;
+        private readonly ILogger _logger;
+
+        public IndexModel(
+            IHostingEnvironment env,
+            IOptions<AppSettings> appSettings,
+            ILogger<IndexModel> logger)
+        {
+            _appSettings = appSettings.Value;
+            _env = env;
+            _logger = logger;
+        }
+
+        public string EnvironmentName { get; set; }
+
+        public AppSettings AppSettings { get; set; }
+
+        public IActionResult OnGet()
+        {
+            AppSettings = _appSettings;
+            EnvironmentName = _env.EnvironmentName;
+
+            return Page();
+        }
+    }
+}
