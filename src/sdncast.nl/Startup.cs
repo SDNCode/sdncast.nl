928524e90730aef66ba1fe3b8635b796a2d596f0
Fanie Reynders <fanie@reynders.co>
2019-04-03 21:44:28 +0200

    Updated something

--- src/sdncast.nl/Startup.cs
+++ src/sdncast.nl/Startup.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved. 
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-//fanie was here
+//fanie was here again
 using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
