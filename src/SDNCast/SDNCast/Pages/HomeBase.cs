2379adc8a8b68815f9cacf7c99e26d96fb2b1cb9
Maarten van Stam <maarten@vanstam.org>
2019-12-27 23:20:36 +0100

    Created Playlist Blazor Components to be reused on various Live Coding Pages. Also moved code to code behind files.

---
+++ src/SDNCast/SDNCast/Pages/HomeBase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+
+using SDNCast.Models;
+using SDNCast.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDNCast.Pages
+{
+    public class HomeBase : ComponentBase
+    {
+        [Inject]
+        ILiveShowDetailsService liveShowDetailsService { get; set; }
+
+        [Inject]
+        IObjectMapper mapper { get; set; }
+
+        [Inject]
+        IShowsService showsService { get; set; }
+
+        [Inject]
+        IOptions<AppSettings> appSettings { get; set; }
+
+        [Inject]
+        AuthenticationStateProvider authenticationStateProvider { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        internal bool showPreviousShows;
+        internal bool showMoreShowsUrl;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+            var uri = new Uri(NavigationManager.Uri);
+            QueryHelpers.ParseQuery(uri.Query).TryGetValue("disableCache", out var disableCacheString);
+            bool.TryParse(disableCacheString, out bool disableCache);
+
+            var liveShowDetails = await liveShowDetailsService.LoadAsync();
+            string playlist = appSettings.Value.YouTubeCastPlaylistId;
+            var showList = await showsService.GetRecordedShowsAsync(user, disableCache, playlist);
+
+            mapper.Map(liveShowDetails, banner);
+            mapper.Map(showList, this);
+            showPreviousShows = PreviousShows.Count() > 0;
+            showMoreShowsUrl = !string.IsNullOrEmpty(MoreShowsUrl);
+        }
+
+        internal HeroBannerModel banner = new HeroBannerModel();
+
+        public IEnumerable<Show> PreviousShows { get; set; }
+        public string MoreShowsUrl { get; set; }
+    }
+}
